043525cd90430660ad182711cb3743066d34f9a3
Serhii Kandyba <s.kandyba@gmail.com>
2020-04-24 19:31:31 +0300

    Added comments for input context methods.

--- src/main/java/calculator/InputContext.java
+++ src/main/java/calculator/InputContext.java
@@ -1,11 +1,24 @@
 package calculator;
 
 public interface InputContext {
+
   boolean isEmpty();
+
   boolean isStopped();
 
+  /**
+   * Set current String line for input context.
+   * Pass null to switch input context state to stopped.
+   */
   void setLine(String line);
-  
+
+  /**
+   * Retrieves next character from line
+   */
   Character getNextChar();
+
+  /**
+   * Retrieves next character from line and moves internal pointer to next character of available.
+   */
   Character fetchNextChar();
 }
